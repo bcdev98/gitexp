fd6d47e59e070529875c23f41b3bb54487001b23
cuib98 <brettcui@yahoo.com>
2017-12-11 21:21:33 -0500

    initial check in

---
+++ src/README.md
@@ -0,0 +1,3 @@
+source file folder
+
+This folder contains program sources or scripts
---
+++ src/my_square.py
@@ -0,0 +1,16 @@
+def my_square(y):
+    """Take a value and returns the squared value
+
+    uses the ** operator
+    """
+    return (y ** 2)
+
+
+def my_square2(x):
+    """uses the * operator to calculate square
+    """
+
+    return (x * x)
+
+print(my_square(42))
+print(my_square2(42))
